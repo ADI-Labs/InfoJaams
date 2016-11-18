b2aacd43f48a33e46d2ac07799513b7e8ada88b1
Sam Kececi <SamKececi@dyn-129-236-215-182.dyn.columbia.edu>
2016-11-18 14:04:52 -0500

    Demo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,45 @@
 class UsersController < ApplicationController
   
   def new
-  	puts params["controller"]
+  	# params[name, password, email, phoneNum]
+
+  	# User.new(info from params)
+  	# if User.save 
+  	# => save it 
+  	# else 
+  	# return some error 
 
   end
+
+
+  # def search 
+  # 	@currentUser = User.find_by(:name => params[name])
+
+  # 	# search = Search.search(params["text"])
+
+  #  	User.update_attribute(:search => search)
+
+  #  	User.save
+
+  # end
+
+
+  # def getHealth 
+  	
+  # 	@currentUser = User.find_by(:name => params[name])
+
+
+  # 	data = @currentUser.healthNews
+
+
+  # 	for each datapoint in data :
+
+  # 		chartkick.graph(datapoint)
+
+
+
+
+  # end
+
 
 end
